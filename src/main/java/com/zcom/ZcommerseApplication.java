1a19385b7700e02deadf32c63a6835f6358a3b3b
anwar mulla <007anwarmulla@gmail.com>
2026-06-30 06:55:36 +0530

    refactor: update pom.xml with additional dependencies and formatting improvements

--- src/main/java/com/zcom/ZcommerseApplication.java
+++ src/main/java/com/zcom/ZcommerseApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ZcommerseApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ZcommerseApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ZcommerseApplication.class, args);
+    }
 
 }
