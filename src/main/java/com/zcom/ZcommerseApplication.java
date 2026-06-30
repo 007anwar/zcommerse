e504a7f8fb0ea5782830af4091ceba91c499401a
anwar mulla <007anwarmulla@gmail.com>
2026-06-30 06:53:55 +0530

    initial commit

---
+++ src/main/java/com/zcom/ZcommerseApplication.java
@@ -0,0 +1,13 @@
+package com.zcom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZcommerseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZcommerseApplication.class, args);
+	}
+
+}
